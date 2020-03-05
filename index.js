e3621822df8330f165bcc24ee1a76d76933a04a1
Jannis R <mail@jannisr.de>
2020-03-05 15:51:40 +0100

    implementation

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
 'use strict'
 
-// todo
+const {strictEqual: eql} = require('assert')
+// Normalize handles umlauts nicely, slugg removes all non-ascii
+// characters nicely, so we use both.
+const normalize = require('normalize-for-search')
+const slugg = require('slugg')
+const municipalities = require('./municipalities.json')
+const removeRegion = require('./lib/remove-region')
+
+// remove `(b. …)`
+const removeBei = (_) => {
+	return _.replace(/(\s+|\b)\(b\.?\s+[^)]+\)/g, '')
+}
+eql(removeBei('foo b. bar baz'), 'foo b. bar baz')
+eql(removeBei('foo (b.) baz'), 'foo (b.) baz')
+eql(removeBei('foo (b.bar) baz'), 'foo (b.bar) baz')
+eql(removeBei('foo (b. bar) baz'), 'foo baz')
+eql(removeBei('foo (b bar) baz'), 'foo baz')
+eql(removeBei('foo (b. bar)'), 'foo')
+
+const findMunicipality = (_) => {
+	for (const municipality of municipalities) {
+		const fromI = _.indexOf(municipality)
+		if (fromI === -1) continue
+
+		// check word boundaries
+		if (fromI !== 0 && _[fromI - 1] !== '-') continue
+		const toI = fromI + municipality.length - 1
+		if (toI !== (_.length - 1) && _[toI + 1] !== '-') continue
+
+		return {municipality, fromI, toI}
+	}
+	return null
+}
+
+const normalizeStopName = (name) => {
+	let _ = normalize(name)
+	_ = removeRegion(removeBei(_))
+	_ = slugg(_)
+
+	// put municipalities last
+	const res = findMunicipality(_)
+	if (res && (res.fromI !== 0 || res.toI !== (_.length - 1))) {
+		_ = [
+			_.slice(0, Math.max(res.fromI - 1, 0)), // text before
+			_.slice(res.toI + 2), // text after
+			'-', res.municipality
+		].join('')
+	}
+
+	_ = _.split('-')
+
+	// todo
+	return _
+}
+
+module.exports = normalizeStopName
--- test.js
+++ test.js
@@ -1,5 +1,20 @@
 'use strict'
 
-const normalize = require('.')
+const {deepStrictEqual: eql, ok} = require('assert')
+const n = require('.')
 
-// todo
+// umlauts
+eql(n('Landstraße ä–$'), ['landstrasse', 'ae'])
+
+// // removes `(Saale)`, `(Elbe)`, etc
+ok(!n('Halle  (Saale), Hallmarkt').includes('saale'))
+ok(!n('Schönebeck  (Elbe), Bahnhof').includes('elbe'))
+eql(n('Foo (Mark)'), ['foo'])
+
+// removes `(b. …)`
+eql(n('Coswig (b Dresden)'), ['coswig'])
+eql(n('Bornstedt (b. Lu. Eisleben), Neuglück'), ['neuglueck', 'bornstedt'])
+
+// puts municipalities last
+eql(n('Osterburg (Altmark), Raiffeisen'), ['raiffeisen', 'osterburg'])
+eql(n('Groß Kreutz'), ['gross', 'kreutz'])
